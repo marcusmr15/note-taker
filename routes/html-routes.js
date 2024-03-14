13a596125f12038d2f1114cfc2e4677c65312ec4
Testing Wizard <marcosmunozr15@gmail.com>
2024-03-13 22:40:54 -0600

    Added code to api-routes.js and html-routes.js files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,1 +1,21 @@
 
+const express = require('express');
+const path = require('path');
+
+// Create a new Router instance
+const router = express.Router();
+
+// Route to serve 'index.html' when a GET request is made to '/'
+router.get('/', (req, res) => {
+    // Send 'index.html' as the response
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Route to serve 'notes.html' when a GET request is made to '/notes'
+router.get('/notes', (req, res) => {
+    // Send 'notes.html' as the response
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// Export the router to be used by other parts of the application
+module.exports = router;
